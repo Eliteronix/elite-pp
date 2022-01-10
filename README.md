01c7d629d5d6bad6ba681def7153549e837f9000
Marvin Zimmermann <64707347+Eliteronix@users.noreply.github.com>
2022-01-10 01:21:02 +0100

    Did stuff I guess

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # elite-pp
  An osu-pp javascript rewrite as a module
+
+Heavily work in progress and incomplete. This will take a while as a cpp noob for now.
--- beatmap.js
+++ beatmap.js
@@ -2,36 +2,8 @@
 // {
 // public:
 
-// 	static bool ContainsAttribute(const std::string &difficultyAttributeName)
-// 	{
-// 		return s_difficultyAttributes.find(difficultyAttributeName) != s_difficultyAttributes.end();
-// 	}
-
-// 	static EDifficultyAttributeType DifficultyAttributeFromName(const std::string &difficultyAttributeName)
-// 	{
-// 		return s_difficultyAttributes.at(difficultyAttributeName);
-// 	}
-
 // private:
-// 	static const std::unordered_map<std::string, EDifficultyAttributeType> s_difficultyAttributes;
 
-// 	// General information
-// 	s32 _id;
-// 	EGamemode _mode = EGamemode::Osu;
-
-// 	// Calculated difficulty
-// 	using difficulty_t = std::unordered_map<
-// 		std::underlying_type_t<EMods>,
-// 		std::array<f32, NumTypes>>;
-
-// 	difficulty_t _difficulty;
-
-// 	// Additional info required for processor
-// 	ERankedStatus _rankedStatus;
-// 	EScoreVersion _scoreVersion;
-// 	s32 _numHitCircles = 0;
-// 	s32 _numSliders = 0;
-// 	s32 _numSpinners = 0;
 // };
 
 export class Beatmap {
@@ -68,6 +40,39 @@ export class Beatmap {
 			ScoreV1 = 1,
 			ScoreV2 = 2,
 		};
+
+		this.s_difficultyAttributes;
+
+		// var list_of_stocks = {
+		// 	"CBB": "Communications",
+		// 	"VZ": "Communications",
+		// 	"UPS": "Transportation",
+		// 	"RRTS": "Transportation",
+		// 	"AAC": "Heath"
+		// };
+		// for (var key in list_of_stocks) {
+		// 	if (list_of_stocks.hasOwnProperty(key)) {
+		// 		console.log(key + " -> " + list_of_stocks[key]);
+		// 	}
+		// }
+
+		// // General information
+		// this._id;
+		// _mode = EGamemode:: Osu;
+
+		// // Calculated difficulty
+		// using difficulty_t = std:: unordered_map <
+		// std:: underlying_type_t < EMods >,
+		// 	std:: array < f32, NumTypes >>;
+
+		// difficulty_t _difficulty;
+
+		// // Additional info required for processor
+		// ERankedStatus _rankedStatus;
+		// EScoreVersion _scoreVersion;
+		// this._numHitCircles = 0;
+		// this._numSliders = 0;
+		// this._numSpinners = 0;
 	}
 
 	// Getter
@@ -95,7 +100,7 @@ export class Beatmap {
 		return _numSpinners;
 	}
 
-	get DifficultyAttribute(mods, type);
+	get DifficultyAttribute(mods, type) { };
 
 	// Method
 	setRankedStatus(rankedStatus) {
@@ -118,10 +123,18 @@ export class Beatmap {
 		_numSpinners = numSpinners;
 	}
 
-	setDifficultyAttribute(mods, type, value);
+	setDifficultyAttribute(mods, type, value) { };
 
 	setMode(mode) {
 		_mode = mode;
+	}
+
+	ContainsAttribute(difficultyAttributeName) {
+		return s_difficultyAttributes.find(difficultyAttributeName) != s_difficultyAttributes.end();
+	}
+
+	DifficultyAttributeFromName(difficultyAttributeName) {
+		return s_difficultyAttributes.at(difficultyAttributeName);
 	}
 
 }
